ae102491141b33ce27b8e3de88501e984acc2ba3
Shekhar Singh <er.shekhar765@gmail.com>
2025-07-30 16:22:11 +0530

    Update Two Sum II - Input Array Is SortedTwoSumII.java

--- 167. Two Sum II - Input Array Is SortedTwoSumII/Two Sum II - Input Array Is SortedTwoSumII.java
+++ 167. Two Sum II - Input Array Is SortedTwoSumII/Two Sum II - Input Array Is SortedTwoSumII.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int i = 0;
-        int j = numbers.length-1;
+        int l = 0;
+        int r = numbers.length-1;
 
-        while(i<j) {
-            if(numbers[i] + numbers[j] == target) {
-                return new int[]{i+1,j+1};
-            } else if (numbers[i] + numbers[j] < target) {
-                i++;
+        while(l < r) {
+            if(numbers[l] + numbers[r] == target) {
+                return new int[]{l+1,r+1};
+            } else if (numbers[l] + numbers[r] < target) {
+                l++;
             } else {
-                j--;
+                r--;
             }
         }
         return null;
