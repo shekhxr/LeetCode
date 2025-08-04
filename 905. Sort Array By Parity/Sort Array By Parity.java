20fc257d69c87c4a28021a0c9b98241361e7f358
Shekhar Singh <er.shekhar765@gmail.com>
2025-08-04 22:55:32 +0530

    Create Sort Array By Parity.java

---
+++ 905. Sort Array By Parity/Sort Array By Parity.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int write = 0;
+
+        for(int read=0;read<nums.length;read++) {
+            if(nums[read] % 2 == 0) {
+                int temp = nums[write];
+                nums[write] = nums[read];
+                nums[read] = temp;
+                write++;
+            }
+        }
+
+        return nums;
+    }
+}
