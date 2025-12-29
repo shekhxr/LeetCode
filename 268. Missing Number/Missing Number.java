6511cd8a7141552e101c4a9fc9d3452bb6731e1a
Shekhar Singh <er.shekhar765@gmail.com>
2025-12-30 00:04:34 +0530

    Refactor missingNumber method for efficiency
    Simplify missing number calculation using XOR.

--- 268. Missing Number/Missing Number.java
+++ 268. Missing Number/Missing Number.java
@@ -1,15 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int xor1 = 0;
-        for(int i = 1; i <= nums.length; i++) {
-            xor1 = xor1 ^ i;
+        int xor = nums.length;  
+        
+        for(int i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];  
         }
         
-        int xor2 = 0;
-        for(int num : nums) {
-            xor2 = xor2 ^ num;
-        }
-
-        return xor1 ^ xor2;
+        return xor;
     }
 }
