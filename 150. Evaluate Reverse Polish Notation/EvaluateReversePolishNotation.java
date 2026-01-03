81b6fd81e5e72b941880aacd6dd4fc15f74d435f
Shekhar Singh <er.shekhar765@gmail.com>
2026-01-03 14:41:36 +0530

    Refactor evalRPN method to simplify operator handling

--- 150. Evaluate Reverse Polish Notation/EvaluateReversePolishNotation.java
+++ 150. Evaluate Reverse Polish Notation/EvaluateReversePolishNotation.java
@@ -1,31 +1,29 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack <Integer> s = new Stack<>();
-
-        for(String token : tokens) {
-            if(isOperator(token)) {
-                int b = s.pop(); //Second Operand
-                int a = s.pop(); //First Operand
-                s.push(applyOperator(token, a, b));
+        Stack<Integer> st = new Stack<>();
+        
+        for(String token : tokens) {  
+            if(token.equals("+")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.push(b + a);
+            } else if(token.equals("-")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.push(b - a);
+            } else if(token.equals("*")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.push(b * a);
+            } else if(token.equals("/")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.push(b / a);
             } else {
-                s.push(Integer.parseInt(token));
+                st.push(Integer.parseInt(token));
             }
         }
-        return s.pop();
-    }
-
-    private boolean isOperator(String token) {
-        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
-    }
-
-    private int applyOperator(String operator, int a, int b) {
-        switch (operator) {
-            case "+" : return a + b;
-            case "-" : return a - b;
-            case "*" : return a * b;
-            case "/" : return a / b;
-            default : throw new IllegalArgumentException("Invalid Operator");
-        }
-
+        
+        return st.pop();
     }
 }
