f4a6973e53a2712f43c644d1b14b5a917ff3809b
Shekhar Singh <er.shekhar765@gmail.com>
2026-02-17 23:30:40 +0530

    Replace generateSubsets with genSubsets method
    Refactored subset generation method to genSubsets.

--- 78. Subsets/Subsets.java
+++ 78. Subsets/Subsets.java
@@ -1,16 +1,22 @@
 class Solution {
+    public static void genSubsets(int nums[], int i, List<Integer> ans, List<List<Integer>> result) {
+        if(i == nums.length) {
+            result.add(new ArrayList<>(ans));
+            return;
+        }
+
+        //Include
+        ans.add(nums[i]);
+        genSubsets(nums, i + 1, ans, result);
+
+        //Exclude
+        ans.remove(ans.size() - 1);
+        genSubsets(nums, i + 1, ans, result);
+    }
+    
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        generateSubsets(0, nums, new ArrayList<>(), result);
+        genSubsets(nums, 0, new ArrayList<>(), result);
         return result;
-    }
-
-    private void generateSubsets(int index, int nums[], List<Integer> current, List<List<Integer>> result) {
-        result.add(new ArrayList<>(current));
-        for(int i=index;i<nums.length;i++) {
-            current.add(nums[i]);
-            generateSubsets(i+1, nums, current, result);
-            current.remove(current.size() - 1);
-        }
     }
 }
