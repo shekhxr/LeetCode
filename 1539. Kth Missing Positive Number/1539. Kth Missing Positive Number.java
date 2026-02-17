f949e82fc50b2c416199faebe53506fefec0badd
Shekhar Singh <er.shekhar765@gmail.com>
2026-02-17 11:24:56 +0530

    Implement findKthPositive method in Solution class

---
+++ 1539. Kth Missing Positive Number/1539. Kth Missing Positive Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0, high = arr.length - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            int missed = arr[mid] - (mid + 1);
+
+            if(missed < k) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return k + low;
+    }
+}
